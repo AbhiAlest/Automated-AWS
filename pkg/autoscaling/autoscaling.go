ffee496335e617a8e6b1a957d35c2dcc308d560b
AbhiAlest <128109884+AbhiAlest@users.noreply.github.com>
2023-06-07 23:03:55 -0500

    Update autoscaling.go
    With tags and suspending/resuming auto scaling processes.

--- pkg/autoscaling/autoscaling.go
+++ pkg/autoscaling/autoscaling.go
@@ -1,11 +1,13 @@
 package autoscaling
 
 import (
+	"fmt"
 	"github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/aws/session"
 	"github.com/aws/aws-sdk-go/service/autoscaling"
 	"github.com/AbhiAlest/Automated-AWS/config"
 )
+
 
 func CreateAutoScalingGroup() {
 	sess, err := session.NewSession(&aws.Config{
@@ -17,7 +19,66 @@ func CreateAutoScalingGroup() {
 
 	svc := autoscaling.New(sess)
 
-	// Implement your logic to create an autoscaling group
-	// using the svc object and the necessary input parameters
+	// Input parameters for creating an Auto Scaling group
+	input := &autoscaling.CreateAutoScalingGroupInput{
+		AutoScalingGroupName: aws.String("MyAutoScalingGroup"), // Replace with the desired name
+		LaunchTemplate: &autoscaling.LaunchTemplateSpecification{
+			LaunchTemplateName: aws.String("MyLaunchTemplate"), // Replace with the desired launch template name
+			Version:            aws.String("$Latest"),
+		},
+		MinSize: aws.Int64(1),
+		MaxSize: aws.Int64(5),
+		DesiredCapacity: aws.Int64(2),
+		VPCZoneIdentifier: aws.String("subnet-12345678"), // Replace with the desired subnet ID
+		Tags: []*autoscaling.Tag{
+			{
+				Key:               aws.String("Environment"),
+				Value:             aws.String("Production"),
+				PropagateAtLaunch: aws.Bool(true),
+			},
+			{
+				Key:               aws.String("Project"),
+				Value:             aws.String("MyProject"),
+				PropagateAtLaunch: aws.Bool(true),
+			},
+		},
+	}
+
+	result, err := svc.CreateAutoScalingGroup(input)
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Println("Auto Scaling group created:", *result.AutoScalingGroupARN)
+
+	// Suspend Auto Scaling Processes
+	suspendInput := &autoscaling.ScalingProcessQuery{
+		AutoScalingGroupName: aws.String("MyAutoScalingGroup"), // Replace with the name of your Auto Scaling group
+		ScalingProcesses: []*string{
+			aws.String("AlarmNotification"),
+		},
+	}
+
+	_, err = svc.SuspendProcesses(suspendInput)
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Println("Auto Scaling processes suspended")
+
+	// Resume Auto Scaling Processes
+	resumeInput := &autoscaling.ScalingProcessQuery{
+		AutoScalingGroupName: aws.String("MyAutoScalingGroup"), // Replace with the name of your Auto Scaling group
+		ScalingProcesses: []*string{
+			aws.String("AlarmNotification"),
+		},
+	}
+
+	_, err = svc.ResumeProcesses(resumeInput)
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Println("Auto Scaling processes resumed")
 }
 
