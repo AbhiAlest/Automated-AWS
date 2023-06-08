e51c1d4e42daca7b4d25e57b3dcf45a2cfa268e4
AbhiAlest <128109884+AbhiAlest@users.noreply.github.com>
2023-06-07 23:09:37 -0500

    Update aws.go

--- config/aws.go
+++ config/aws.go
@@ -1,6 +1,16 @@
 package config
 
-const (
-    AWSRegion      = "us-west-2" // Replace with your desired region
-    AWSCredentials = "default"   // Replace with your desired credential profile
+import (
+	"github.com/aws/aws-sdk-go/aws/credentials"
 )
+
+const (
+	AWSRegion      = "us-west-2" // Replace with your desired region
+	AWSCredentials = "default"   // Replace with your desired credential profile
+)
+
+// GetAWSCredentials returns the AWS credentials based on the specified profile
+func GetAWSCredentials() *credentials.Credentials {
+	creds := credentials.NewSharedCredentials("", AWSCredentials)
+	return creds
+}
