eeabeee777b8cf3ce8e81053ea68ae3a6cfa52e2
AbhiAlest <128109884+AbhiAlest@users.noreply.github.com>
2023-06-07 23:07:50 -0500

    Update main.go

--- cmd/main.go
+++ cmd/main.go
@@ -1,1 +1,15 @@
+package main
+
+import (
+	"github.com/AbhiAlest/Automated-AWS/pkg/ec2"
+	"github.com/AbhiAlest/Automated-AWS/pkg/elbv2"
+	"github.com/AbhiAlest/Automated-AWS/pkg/autoscaling"
+)
+
+func main() {
+	// Call the functions to create AWS resources
+	ec2.CreateEC2Instance()
+	elbv2.CreateLoadBalancer()
+	autoscaling.CreateAutoScalingGroup()
+}
 
