2c8a3ad1e520c0f8d5a081ccd6df8585ed6cf52d
AbhiAlest <128109884+AbhiAlest@users.noreply.github.com>
2023-06-07 21:52:48 -0500

    Update ec2.go

--- pkg/ec2/ec2.go
+++ pkg/ec2/ec2.go
@@ -1,1 +1,23 @@
+package ec2
+
+import (
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/ec2"
+	"github.com/your-username/your-tool/config"
+)
+
+func CreateEC2Instance() {
+	sess, err := session.NewSession(&aws.Config{
+		Region: aws.String(config.AWSRegion),
+	})
+	if err != nil {
+		panic(err)
+	}
+
+	svc := ec2.New(sess)
+
+	// Implement your logic to create an EC2 instance
+	// using the svc object and the necessary input parameters
+}
 
