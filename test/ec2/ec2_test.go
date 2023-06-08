04be366f868bd69461f5be3858f346c26b09698a
AbhiAlest <128109884+AbhiAlest@users.noreply.github.com>
2023-06-07 21:55:48 -0500

    Update ec2_test.go

--- test/ec2/ec2_test.go
+++ test/ec2/ec2_test.go
@@ -1,1 +1,13 @@
+package ec2_test
+
+import (
+	"testing"
+
+	"github.com/your-username/your-tool/pkg/ec2"
+)
+
+func TestCreateEC2Instance(t *testing.T) {
+	// Write your test case for creating an EC2 instance
+	// using the ec2.CreateEC2Instance() function
+}
 
